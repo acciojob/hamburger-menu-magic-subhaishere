06ca3d429dd2ea072338e4117ee92fea32c65b51
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 00:41:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
